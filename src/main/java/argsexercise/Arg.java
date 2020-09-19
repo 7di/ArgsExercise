9703cea5dbba988315bb740f7c7c4f10e7526aef
Cs <1453749520@qq.com>
2020-09-19 22:41:03 +0800

    ArgsExericise-All New Code Commit

--- src/main/java/argsexercise/Arg.java
+++ src/main/java/argsexercise/Arg.java
@@ -13,8 +13,12 @@ public class Arg {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Arg that = (Arg) o;
         return Objects.equals(key, that.key) &&
                 Objects.equals(value, that.value);
